831dfe47916740880950def5daffef6c0e303f61
Jetnipat-c <alonecheer@hotmail.com>
2023-08-15 09:49:13 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { authOptions } from "pages/api/auth/[...nextauth]";
 const inter = Inter({ subsets: ["latin"] });
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
+  //   const session = await getServerSession(authOptions);
   return (
     <div>Home page</div>
     // <main className={styles.main}>
