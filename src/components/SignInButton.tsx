4844934ac1b5514a1ca0d1c7588ee682343fef9b
Jetnipat-c <alonecheer@hotmail.com>
2023-08-16 12:00:39 +0700

    update

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -6,7 +6,10 @@ const SignInButton = () => {
   return (
     <div>
       <button
-        onClick={() => signIn("cognito", { callbackUrl: "/" })}
+        onClick={(e) => {
+          e.preventDefault();
+          signIn("cognito", { callbackUrl: "/" });
+        }}
         className="bg-blue-400 p-2 text-white rounded-md hover:bg-blue-500 w-full"
       >
         Sign in
